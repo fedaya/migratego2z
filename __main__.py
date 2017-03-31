f95812722db250ab382757e6d4b02b9ff91988f4
Etienne GILLE <etienne.gille@ville-acigne.fr>
2017-03-31 11:24:24 +0200

    Still 0.9.0 release
    - Intermediate commit to remove some file from the repo
    - migratego2z.ini is now a generic one

--- __main__.py
+++ __main__.py
@@ -9,8 +9,10 @@ def main():
                         default=[None])
     parser.add_argument('--config', '-c', help='configuration file for import', nargs=1,
                         default=['/etc/migratego2z/migratego2z.ini'])
+    parser.add_argument('--exclude', '-x', help='exlucde some users', default=[None], nargs='+')
+    parser.add_argument('--user', '-u', help='extract for for a particuliar user', nargs=1, default=[None])
     args = parser.parse_args()
-    prog = migratego2z.Main(args.rootDir[0], args.domain[0], args.config[0])
+    prog = migratego2z.Main(args.rootDir[0], args.domain[0], args.config[0], args.exclude, args.user[0])
     prog.main()
 
 
